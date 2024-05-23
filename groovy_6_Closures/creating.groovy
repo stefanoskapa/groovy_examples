2c81f199db1ef191f9fd4d498f85b39358047fb1
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2024-05-23 20:12:44 +0300

    Closures

---
+++ groovy_6_Closures/creating.groovy
@@ -0,0 +1,16 @@
+/*
+Closures are probably the key-feature that
+made Groovy so popular
+
+A closure in Groovy is an open, anonymous, 
+block of code that can take arguments, return 
+a value and be assigned to a variable. A closure 
+may reference variables declared in its surrounding 
+scope. In opposition to the formal definition of a 
+closure, Closure in the Groovy language can also 
+contain free variables which are defined outside 
+of its surrounding scope.
+
+
+
+*/
