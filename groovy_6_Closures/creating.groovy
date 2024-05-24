f087b2ebdfad50c793b3a5a4adc125d4a09bbad8
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2024-05-24 21:32:48 +0300

    creating closures

--- groovy_6_Closures/creating.groovy
+++ groovy_6_Closures/creating.groovy
@@ -1,16 +1,60 @@
-/*
-Closures are probably the key-feature that
-made Groovy so popular
+def c = {}
 
-A closure in Groovy is an open, anonymous, 
-block of code that can take arguments, return 
-a value and be assigned to a variable. A closure 
-may reference variables declared in its surrounding 
-scope. In opposition to the formal definition of a 
-closure, Closure in the Groovy language can also 
-contain free variables which are defined outside 
-of its surrounding scope.
+println c.class.name
+println c instanceof Closure
 
+//a closure without arguments
+def sayHello = {
+  println "hello"
+}
+
+sayHello()
 
 
+/* 
+a closure with arguments
+We separate the args from the method body with
+the -> operator
 */
+def sayHelloName = { name ->
+  println "Hello, $name"
+}
+
+sayHelloName("Bob")
+
+/*
+An important difference between closures and methods, is that
+closures can be passed around.
+*/
+
+def nums = [1,2,3,2,4,6,9,8]
+
+nums.each({
+  println it
+})
+
+def timesFive(num, closure) {
+  closure(num * 5)
+}
+
+timesFive(10, { println it })
+
+/*
+Any method that accepts a closure as its last
+parameter, can be written as follows
+*/
+timesFive(11) {
+  println it
+}
+
+
+def dependencies(Closure closure) {
+  closure()
+}
+
+dependencies {
+  println "dependency"
+}
+
+10.times { println 'Hello'}
+
